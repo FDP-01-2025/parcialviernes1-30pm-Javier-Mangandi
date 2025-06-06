7d403aa697cd868f31f6cb3ec9b9d9528a987d61
Javier-Mangandi <00204125@uca.edu.sv>
2025-06-06 14:15:37 -0600

    update

--- 02ExerciseArreglos.cpp
+++ 02ExerciseArreglos.cpp
@@ -1,32 +1,31 @@
 /*
-Pida al usuario N cantidad de numeros y guardelos en un arreglo, identificar cual es el menor numero en el arreglo
+Pida al usuario N cantidad de numeros y guardelos en un arreglo, identificar
+cual es el menor numero en el arreglo
 */
 #include <iostream>
 using namespace std;
-int main (){
-    int array[100]; //Colocamos un limite para que el usuario no se exceda
-    int minimo;
-    int n;
+int main() {
+  int array[100];  // Colocamos un limite para que el usuario no se exceda
+  int minimo;
+  int n;
 
-    cout<<"Por favor indice una cantidad de numeros que le gustaria añadir a su arreglo:"<<endl;
-    cout<<"(Maximo 100 y Minimo 1)"<<endl;
-    cin>>n;
+  cout << "Por favor indice una cantidad de numeros que le gustaria añadir a "
+          "su arreglo:"
+       << endl;
+  cout << "(Maximo 100 y Minimo 1)" << endl;
+  cin >> n;
 
-    for(int i=0;i<n;i++){
-        cout<<"Numero #"<<i+1<<":  ";
-        cin>>array[i];
+  for (int i = 0; i < n; i++) {
+    cout << "Numero #" << i + 1 << ":  ";
+    cin >> array[i];
 
-        if(array[i] < minimo){
-            minimo = array[i];
-        }
-
+    if (array[i] < minimo) {
+      minimo = array[i];
     }
-    
-    cout<<"El elemento minimo en su arreglo es:"<<minimo<<endl;
-    cout<<"============================================="<<endl;
+  }
 
+  cout << "El elemento minimo en su arreglo es:" << minimo << endl;
+  cout << "=============================================" << endl;
 
-
-
-    return 0;
+  return 0;
 }
